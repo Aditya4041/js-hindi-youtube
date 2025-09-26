861c56d8e0011be143eba202f7b9165d4b560f68
Aditya_Suryawanshi <adityasuryawanshi5749@gmail.com>
2025-09-26 06:33:57 +0000

    datatypes

--- 01_basics/02_DataTypes.js
+++ 01_basics/02_DataTypes.js
@@ -5,3 +5,23 @@
 console.log(3+3);// code readability should be high
 console.log("aditya");
 
+let name = "aditya"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// primitive datatypes
+// number => 2 topower 53
+// bigint
+// string => ""
+// boolean => true/false
+// null=> standalone value
+// undefined => 
+// symbol => unique
+
+//objest
+console.log(typeof name);
+console.log(typeof null)// object
+console.log(typeof undefined);// undefined
+
+
