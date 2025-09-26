e54333fe7bacc5abb35473d35f0b4692fbf9429e
Aditya_Suryawanshi <adityasuryawanshi5749@gmail.com>
2025-09-26 07:19:18 +0000

    conversion of datatypes

---
+++ 01_basics/03.js
@@ -0,0 +1,16 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score) // convert string into number
+console.log(typeof valueInNumber);
+// conversion
+// "33"=> 33
+// "33abc" => non as number
+// true => 1; false => 0;
+
+let isloggedIn = 1
+let booleanIsLoggedIn = Boolean(isloggedIn) // convert number into boolean( 1 into  true and 0 into false)
+console.log(booleanIsLoggedIn);
+ // "" => false;
+ // "aditya" => true
