19179740f5285613f57c337f40af8fe51f5a90fa
Aditya_Suryawanshi <adityasuryawanshi5749@gmail.com>
2025-09-26 06:13:42 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 5749
+let accountemail = "aditya5749@gmail.com"
+var accountPassword = "12345"
+accountCity = "kolhapur"
+let accountState
+
+// accountId = 2 // not allowed
+
+accountemail = "chinmay123@gmail.com"
+accountPassword = "7755"
+accountCity = "mumbai"
+/*
+not use var 
+because of issue in block scope and function scope
+it is multiline comment
+*/
+
+console.log(accountId);
+console.table([accountId,accountPassword,accountemail,accountCity,accountState])
+
