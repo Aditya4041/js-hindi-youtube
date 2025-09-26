ee48a1306ce3613b3ff9c3269bd8558ff17b9ec7
Aditya_Suryawanshi <adityasuryawanshi5749@gmail.com>
2025-09-26 11:54:27 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,10 @@
+const name = "aditya"
+const count = 50
+
+//console.log(name + count + "value");
+console.log(`my name is ${name} and my repository count is ${count}`);
+
+
+const gamename = new String('aditya')
+console.log(gamename[0]);
+console.log(gamename.__proto__.);
