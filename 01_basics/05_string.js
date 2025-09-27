5e12514af3c0f85cf9a849430d5f1b34b8c616d9
Aditya_Suryawanshi <adityasuryawanshi5749@gmail.com>
2025-09-27 06:00:14 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -2,9 +2,38 @@ const name = "aditya"
 const count = 50
 
 //console.log(name + count + "value");
-console.log(`my name is ${name} and my repository count is ${count}`);
+//console.log(`my name is ${name} and my repository count is ${count}`);
 
 
 const gamename = new String('aditya')
-console.log(gamename[0]);
-console.log(gamename.__proto__.);
+/* console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+
+*/
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('y'));
+
+
+// const newstring = gamename.substring(0, 4)
+// console.log(newstring);
+
+// const anotherstring = gamename.slice(-5, 2)
+// console.log(anotherstring);
+
+// const newstringone = "   aditya   "
+// console.log(newstringone);
+// console.log(newstringone.trim());
+
+const newad = "aditya%20suryawanshi"
+console.log(newad.replace('%20','_'));
+
+console.log(newad.includes("aditya"));
+
+
+
+
+
+
+
