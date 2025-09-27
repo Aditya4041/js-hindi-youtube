1143fc61df6d73b81756fc55ab0169a2821e0b81
Aditya_Suryawanshi <adityasuryawanshi5749@gmail.com>
2025-09-27 08:22:38 +0000

    maths

---
+++ 01_basics/06.js
@@ -0,0 +1,17 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(typeof balance);
+
+
+// const num = 1000000000
+// console.log(num.toLocaleString('en-In'));
+//  ++++++++++++++++++++maths++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+
