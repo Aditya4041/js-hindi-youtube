9078352e1b1bfeb10d978b6c12c37b70c029614a
Aditya_Suryawanshi <adityasuryawanshi5749@gmail.com>
2025-09-27 10:02:20 +0000

    dates

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,1 +1,9 @@
+// let mydate = new Date()
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
 
+let newdate = new Date()
+console.log(newdate.toLocaleString());
