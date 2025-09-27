6dfa8f586c4b8f1096df19c0c896a2c8c8f74957
Aditya_Suryawanshi <adityasuryawanshi5749@gmail.com>
2025-09-27 10:08:19 +0000

    string and dates

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,34 +1,39 @@
-// Dates
+const name = "aditya"
+const count = 50
 
-let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+//console.log(name + count + "value");
+//console.log(`my name is ${name} and my repository count is ${count}`);
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now()
+const gamename = new String('aditya')
+/* console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+*/
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('y'));
 
-let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
 
-// `${newDate.getDay()} and the time `
+// const newstring = gamename.substring(0, 4)
+// console.log(newstring);
 
-newDate.toLocaleString('default', {
-    weekday: "long",
-    
-})
-git pull origin main --rebase
-git push origin main
-git push origin main --force
+// const anotherstring = gamename.slice(-5, 2)
+// console.log(anotherstring);
+
+// const newstringone = "   aditya   "
+// console.log(newstringone);
+// console.log(newstringone.trim());
+
+const newad = "aditya%20suryawanshi"
+console.log(newad.replace('%20','_'));
+
+console.log(newad.includes("aditya"));
+
+
+
+
+
+
+
