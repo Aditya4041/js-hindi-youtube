c148e57dcd6de1cf93c2a7d484a027895d70d4d5
Aditya_Suryawanshi <adityasuryawanshi5749@gmail.com>
2025-09-29 11:38:40 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,48 @@
+function myname(){
+     console.log("a");
+ console.log("d");
+ console.log("i");
+ console.log("t");
+ console.log("y");
+ console.log("a");
+}
+// myname()
+// function addtwonumbers(number1,number2){
+//     console.log(number1+number2);
+// }
+addtwonumbers(2,3) // call the function using function name 
+function addtwonumbers(number1,number2){
+    // let add = number1+number2
+    // return add
+    return number1+number2
+}
+const add = addtwonumbers(3,5)
+// console.log("result:",add);
+
+
+// function userlogin(username){
+//     return `${username} just logged in`
+// }
+
+// console.log(userlogin("aditya"));
+
+
+// function userlogin(username){
+//     if(username===undefined){
+//         console.log("plz enter user name ");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+// console.log(userlogin());
+
+function userlogin(username = "adi"){
+    if(!username){
+        console.log("plz enter user name ");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(userlogin("aditya"));
