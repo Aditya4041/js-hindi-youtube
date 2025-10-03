c4cd3b7404c1d7eb8b824bd1c42d4bafbd78dbac
Aditya_Suryawanshi <adityasuryawanshi5749@gmail.com>
2025-10-03 07:06:04 +0000

    iterations

---
+++ 05_iterations/four.js
@@ -0,0 +1,11 @@
+const myobj = {
+    js:'javascript',
+    cpp:'c++',
+    rb:'rubby',
+    swift : 'swift by apple'
+}
+ 
+for (const key in myobj) {
+    console.log(`full form of ${key} is ${myobj[key]}`);
+    
+}
---
+++ 05_iterations/three.js
@@ -0,0 +1,32 @@
+// for of 
+
+const arr = [1,2,3,4,5]
+for (const num of arr) {
+    // console.log(num);
+}
+
+const adi = "aditya suryawanshi"
+for (const i of adi) {
+    // console.log(i);   
+}
+
+// map 
+const map = new Map()
+map.set ('in',"india")
+map.set('usa',"united state of america")
+map.set('fr',"france")
+// console.log(map);
+
+for (const [key , value] of map) {
+    // console.log(key,`:-`,value);
+    
+}
+
+const myobj = {
+    'game1': "nsf",
+    'game2':"spiderman"
+}
+// for (const [key,value] of myobj) {
+//     console.log(key,`:-`,value);
+    
+// }
