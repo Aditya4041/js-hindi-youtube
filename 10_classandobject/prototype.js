78318b19568fad2c69f85f3d4bbda1a7e7e1c816
aditya suryawanshi <adityasuryawanshi5749@gmail.com>
2025-10-10 16:57:37 +0530

    prototype

--- 10_classandobject/prototype.js
+++ 10_classandobject/prototype.js
@@ -20,4 +20,44 @@ Object.prototype.aditya = function(){
     
 }
 
-heropower.aditya();
+// heropower.aditya();
+// myheros.aditya()
+
+
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "aditya     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"chinmay".trueLength()
+"  yash patil  ".trueLength()
